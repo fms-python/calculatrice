7a95e9e2d6c81671a25adfd788f3a913b186cd09
Martial Bret <martial.bret@fms-ea.com>
2025-10-21 10:53:34 +0200

    Ajout d'un nouveau fichier de départ pour la prise en main de la collaboration avec Git et GitHub pour l'académie CDA Python-Java.

--- calculator.py
+++ calculator.py
@@ -2,32 +2,11 @@
 # -*- coding: utf-8 -*-
 
 """
-Calculatrice à 4 opérations
+Calculatrice
 """
 
-
 def add(op1, op2):
-    return op1 + op2
-
+    ...
 
 def substract(op1, op2):
-    return op1 - op2
-
-
-def multiply(op1, op2):
-    return op1 * op2
-
-
-def divide(op1, op2):
-    return op1 / op2
-
-
-if __name__ == '__main__':
-    print(add(2, 3))
-    print(add(3, 2))
-    print(substract(5, 2))
-    print(substract(2, 5))
-    print(multiply(3, 5))
-    print(multiply(5, 3))
-    print(divide(6, 3))
-    print(divide(3, 6))
+    ...
